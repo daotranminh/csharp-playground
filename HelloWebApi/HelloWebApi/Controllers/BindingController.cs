9b9eb2378bd45b9814b1dcc97511f47dae8e51d7
daotranminh <daotranminh@gmail.com>
2017-03-09 00:31:12 +0100

    Reading a raw HTTP request.

---
+++ HelloWebApi/HelloWebApi/Controllers/BindingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HelloWebApi.Controllers
+{
+    public class BindingController : ApiController
+    {
+        public void Post(HttpRequestMessage req)
+        {
+            string content = req.Content.ReadAsStringAsync().Result;
+            int id = Int32.Parse(req.RequestUri.Segments.Last());
+
+            Trace.WriteLine(content);
+            Trace.WriteLine(id);
+        }
+    }
+}
