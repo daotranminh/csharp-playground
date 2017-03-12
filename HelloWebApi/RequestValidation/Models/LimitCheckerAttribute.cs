0401afc30810320708f57f33e6d7a516c8f8d3ca
daotranminh <daotranminh@gmail.com>
2017-03-13 00:44:33 +0100

    Create your own Validation Attribute (doesn't work)

---
+++ HelloWebApi/RequestValidation/Models/LimitCheckerAttribute.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RequestValidation.Models
+{
+    public class LimitCheckerAttribute : ValidationAttribute
+    {
+        public LimitCheckerAttribute(string baseProperty, double percentage)
+        {
+            this.BaseProperty = baseProperty;
+            this.Percentage = percentage;
+            this.ErrorMessage = "{0} cannot exceed {1}% of {2}";
+        }
+
+        public string BaseProperty { get; set; }
+        public double Percentage { get; set; }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return string.Format(ErrorMessageString, name, Percentage, BaseProperty);
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            decimal amount = (decimal)value;
+
+            // this gives us the AnnualIncome
+            var propertyInfo = validationContext.ObjectType.GetProperty(this.BaseProperty);
+
+            if (propertyInfo != null)
+            {
+                // the amount of the AnnualIncome
+                decimal baseAmount = (decimal)propertyInfo.GetValue(validationContext.ObjectInstance, null);
+
+                decimal maxLimit = baseAmount * (decimal)this.Percentage / 100;
+
+                if (amount < maxLimit) return ValidationResult.Success;
+            }
+
+            return new ValidationResult(FormatErrorMessage(validationContext.DisplayName));
+        }
+    }
+}
