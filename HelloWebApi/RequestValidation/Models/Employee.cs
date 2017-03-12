0a5f42a634a41d2c0e6829b68dde7a0caf4276f3
daotranminh <daotranminh@gmail.com>
2017-03-13 00:13:58 +0100

    Validation using data annotations.

---
+++ HelloWebApi/RequestValidation/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace RequestValidation.Models
+{
+    public class Employee
+    {
+        [Range(10000, 99999)]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string LastName { get; set; }
+
+        [RegularExpression("[0-1][0-9]")]
+        public string Department { get; set; }
+    }
+}
