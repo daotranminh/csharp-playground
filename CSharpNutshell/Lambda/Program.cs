645ec487405ffff73c6b89e820acdd43c4d8fa3b
daotranminh <daotranminh@gmail.com>
2017-03-19 22:30:53 +0100

    Lambda and Iterator.

---
+++ CSharpNutshell/Lambda/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, string, int> totalLength = (s1, s2) => s1.Length + s2.Length;
+            Console.WriteLine(totalLength("hello", "world"));
+
+            int factor = 2;
+            Func<int, int> multiplier = n => n * factor;
+            Console.WriteLine(multiplier(3));
+
+            factor++;
+            Console.WriteLine(multiplier(3))
+;
+            Console.ReadLine();
+        }
+    }
+}
