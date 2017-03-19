94be2be2ef5fe864ead986cce95e831e604d121f
daotranminh <daotranminh@gmail.com>
2017-03-19 21:35:50 +0100

    Event pattern.

---
+++ CSharpNutshell/Event/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stock stock = new Event.Stock("XYZ");
+            stock.Price = 27.19M;
+            stock.PriceChanged += stock_PriceChanged;
+            stock.Price = 40M;
+            Console.ReadLine();
+        }
+
+        static void stock_PriceChanged(object sender, PriceChangedEventArgs e)
+        {
+            if ((e.NewPrice - e.LastPrice) / e.LastPrice > 0.1M)
+                Console.WriteLine("Alert, 10% stock price increased from {0} to {1}", e.LastPrice, e.NewPrice);
+        }
+    }
+
+    public class PriceChangedEventArgs : EventArgs
+    {
+        public readonly decimal LastPrice;
+        public readonly decimal NewPrice;
+
+        public PriceChangedEventArgs(decimal lastPrice,
+                                     decimal newPrice)
+        {
+            LastPrice = lastPrice;
+            NewPrice = newPrice;
+        }
+    }
+
+    public class Stock
+    {
+        string symbol;
+        decimal price;
+
+        public Stock(string symbol)
+        {
+            this.symbol = symbol;
+        }
+
+        public event EventHandler<PriceChangedEventArgs> PriceChanged;
+
+        protected virtual void OnPriceChanged(PriceChangedEventArgs e)
+        {
+            if (PriceChanged != null) PriceChanged(this, e);
+        }
+
+        public decimal Price
+        {
+            get { return price; }
+            set
+            {
+                if (price == value) return;
+                decimal oldPrice = price;
+                price = value;
+                OnPriceChanged(new PriceChangedEventArgs(oldPrice, price));
+            }
+        }
+    }
+}
